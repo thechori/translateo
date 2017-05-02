80e7ba24686268c4502b3c1595ae4eb04cb7d50e
rhteodor <rhteodor@central.uh.edu>
2017-05-02 10:46:36 -0500

    Continues work to TheQuiz for deep link learning

--- src/pages/quiz/thequiz.ts
+++ src/pages/quiz/thequiz.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, NgModule } from '@angular/core';
+import { IonicPage, NavController, IonicPageModule } from 'ionic-angular';
 
 @IonicPage()
 @Component({
   selector: 'the-quiz',
   templateUrl: 'thequiz.html'
 })
+// Note: You can break this module decorator out into the "thequiz.module.ts" file as well
+@NgModule({
+  declarations: [
+    TheQuiz,
+  ],
+  imports: [
+    IonicPageModule.forChild(TheQuiz),
+  ],
+  exports: [
+    TheQuiz
+  ]
+})
 export class TheQuiz {
   constructor() {
     console.log("TheQuiz.constructor()");
   }
 
-  hello() {
-    console.log("hello")
-  }
 }
