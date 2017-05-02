522c1051b34094bb274ca710f26575b82925b41b
rhteodor <rhteodor@central.uh.edu>
2017-05-01 20:07:13 -0500

    Fully working settings page with data that is stored locally using the Storage class

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -8,25 +8,54 @@ import { Storage } from '@ionic/storage';
 })
 export class SettingsPage {
 
-  public defaultLanguage: string;
+  // public firstName: string;
+  // public lastName: string;
+  // public gender: string;
+  // public proMode: boolean;
+  // public defaultLanguage: string;
+
+  public settings: any
 
   constructor(public navCtrl: NavController, public storage: Storage) {
 
-    this.defaultLanguage = "english"
+    // Defaults
+    this.settings = {
+      firstName: "",
+      lastName: "",
+      gender: "",
+      proMode: false,
+      defaultLanguage: ""
+    }
 
+    // When the storage is ready, retrieve the data and set it to the settings object
     storage.ready().then(() => {
       console.log("Storage ready");
 
-      storage.set("name_first", "Ryan");
-      storage.set("name_last", "Teodoro");
+      // Clear storage
+      // storage.clear().then(()=>{console.log("storage clear")})
 
-      storage.get("name_first").then((val) => {
-        console.log("First name: " + val);
-
-
-
-        console.log("done");
+      // Retrieve data from local storage
+      storage.get("settings").then((settings) => {
+        // Check for null settings object
+        if (!settings) {
+          console.log("no settings found..");
+          console.log(settings);
+        } else {
+          this.settings.firstName = settings.firstName,
+          this.settings.lastName = settings.lastName,
+          this.settings.gender = settings.gender,
+          this.settings.proMode = settings.proMode,
+          this.settings.defaultLanguage = settings.defaultLanguage
+        }
       })
+    })
+  }
+
+  // Save data to local storage
+  save() {
+    this.storage.set("settings", this.settings).then((data) => {
+      console.log("settings saved!");
+      console.log(data);
     })
   }
 
