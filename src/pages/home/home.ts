4a7ef5f0f83fef1d42a4a5574185f087cc9f450e
rhteodor <rhteodor@central.uh.edu>
2017-05-03 14:59:57 -0500

    Successfully captures an image and sets it to the img element on the Home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -32,11 +32,17 @@ export class HomePage {
     }
 
     this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64:
-     let base64Image = 'data:image/jpeg;base64,' + imageData;
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log("image:");
+      console.log(base64Image);
+
+      // Populate the image with the data
+      (<HTMLImageElement>document.getElementById('myImage')).src = base64Image;
     }, (err) => {
-     // Handle error
+      // Handle error
+      console.error(err);
     });
   }
 }
