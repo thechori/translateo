2fdd31700d06476a9b3902e912ea7e6346abdb73
rhteodor <rhteodor@central.uh.edu>
2017-05-05 14:46:15 -0500

    Adds the logic for selecting different states within the audio player

--- src/components/audio-player/audio-player.ts
+++ src/components/audio-player/audio-player.ts
@@ -12,27 +12,56 @@ import { Component } from '@angular/core';
 })
 export class AudioPlayer {
 
-  text: string;
+  title: string;
   src: string;
   isPlaying: boolean;
+  isPaused: boolean;
+  isStopped: boolean;
 
   constructor() {
     console.log('Hello AudioPlayer Component');
-    this.text = 'Hello World';
+    this.title = 'Hello World';
     this.src = 'http://incompetech.com/music/royalty-free/mp3-preview2/Ouroboros.mp3';
     this.isPlaying = false;
+    this.isPaused = false;
+    this.isStopped = true;
   }
 
   play() {
     console.log("play");
+
+    if (this.isPlaying) {
+      return;
+    }
+
+    this.isPlaying = true;
+    this.isPaused = false;
+    this.isStopped = false;
   }
 
   pause() {
     console.log("pause");
+
+    if (this.isPaused) {
+      return;
+    }
+
+    this.isPlaying = false;
+    this.isPaused = true;
+    this.isStopped = false;
   }
 
   stop() {
     console.log("stop")
+
+    if (this.isStopped) {
+      return;
+    }
+
+    this.isPlaying = false;
+    this.isPaused = false;
+    this.isStopped = true;
   }
+
 
 }
